f51b579700710f7a91b5fcdfaaf656172ccb179a
obeymyshinnyrod <miswah.md@gmail.com>
2021-09-24 20:01:24 +0530

    Added Navbar for #8
    Navbar without link for now

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,14 +5,25 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CdaService } from './cda.service';
 import { HomepageComponent } from './homepage/homepage.component';
-import { BlogViewComponent } from './blog-view/blog-view.component';
+
 import { BlogListComponent } from './blog-list/blog-list.component';
 import { FeaturedBlogComponent } from './featured-blog/featured-blog.component';
 import { BlogPageComponent } from './blog-page/blog-page.component';
 
 @NgModule({
+<<<<<<< Updated upstream
   declarations: [AppComponent, HomepageComponent, BlogViewComponent, BlogListComponent, FeaturedBlogComponent, BlogPageComponent],
   imports: [BrowserModule, AppRoutingModule],
+=======
+  declarations: [
+    AppComponent,
+    HomepageComponent,
+    BlogListComponent,
+    FeaturedBlogComponent,
+    PostViewComponent,
+  ],
+  imports: [BrowserModule, AppRoutingModule, NgbModule],
+>>>>>>> Stashed changes
   providers: [CdaService],
   bootstrap: [AppComponent],
 })
