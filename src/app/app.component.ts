9d62b9a16d3c1d3101cabdd53ca3b274ffb7422d
obeymyshinnyrod <miswah.md@gmail.com>
2021-10-01 21:45:37 +0530

    [feat] : added footer #27
    Added footer and changed tags color to purple from black and weight to 400 from 100

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, SimpleChanges } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +7,17 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Import-Blog';
+  public loaded: boolean = false;
+
+  ngOnInit(): void {
+    //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
+    //Add 'implements OnInit' to the class.
+    this.loaded = true;
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    //Called before any other lifecycle hook. Use it to inject dependencies, but avoid any serious work here.
+    //Add '${implements OnChanges}' to the class.
+    this.loaded = false;
+  }
 }
