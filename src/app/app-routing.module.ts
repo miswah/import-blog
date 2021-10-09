98f94b514b4e95efb6c02aa13558f73207accbc4
obeymyshinnyrod <miswah.md@gmail.com>
2021-10-09 20:26:33 +0530

    (redo): Overhaul the design of featured-blog-component
    Massive design overhaul for featured blog section

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { BlogCardsComponent } from './views/blog-cards/blog-cards.component';
+import { BlogShowcaseComponent } from './views/blog-showcase/blog-showcase.component';
+import { BlogViewComponent } from './views/blog-view/blog-view.component';
 import { HomepageComponent } from './views/homepage/homepage.component';
 
 const routes: Routes = [
-  { path: 'home', component: BlogCardsComponent },
+  { path: 'home', component: BlogViewComponent },
   { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'view/:id', component: BlogViewComponent },
 ];
 
 @NgModule({
--- src/app/views/blog-showcase/blog-showcase.component.ts
+++ src/app/views/blog-showcase/blog-showcase.component.ts
@@ -1,15 +1,39 @@
 import { Component, OnInit } from '@angular/core';
+import { ContentfulManagmentService } from 'src/app/services/contentful-managment.service';
 
 @Component({
   selector: 'app-blog-showcase',
   templateUrl: './blog-showcase.component.html',
-  styleUrls: ['./blog-showcase.component.css']
+  styleUrls: ['./blog-showcase.component.css'],
 })
 export class BlogShowcaseComponent implements OnInit {
-
-  constructor() { }
+  featuredBlogNo: number = 0;
+  blog: any = [];
+  featuredBlogs: Array<any> = [];
+  featuredBlogId: Number = 0;
+  loaded: boolean = false;
+  constructor(private cdaService: ContentfulManagmentService) {}
 
   ngOnInit(): void {
+    this.getBlogs();
   }
 
+  //getBlogs
+  getBlogs() {
+    this.cdaService.getBlogs().then((data: any) => {
+      //setting featured Blogs
+      for (let i = data.length - 1; i > data.length - 4; i--) {
+        this.featuredBlogs.push(data[i]);
+      }
+      this.blog = this.featuredBlogs[0];
+      console.log(this.featuredBlogs);
+      this.loaded = true;
+    });
+  }
+
+  //fetching the featured blog via click on button
+  fetch(id: number) {
+    this.featuredBlogNo = id;
+    this.blog = this.featuredBlogs[id];
+  }
 }
